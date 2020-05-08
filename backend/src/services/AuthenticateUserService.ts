27877241f65213acc71e3b2711f1ac2d16ed2288
Raul Garcia <raul.gsouza@gmail.com>
2020-05-08 18:36:30 -0300

    Added JWT generation

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -8,8 +9,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 export default class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<{ user: User }> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email } });
@@ -24,6 +30,11 @@ export default class AuthenticateUserService {
       throw Error('Incorrect email/password combination');
     }
 
-    return { user };
+    const token = sign({}, '35ae251d487a83617934048621b5b65a', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
