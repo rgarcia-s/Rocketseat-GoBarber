d811470b804ca652958fa39c934c692f42fb25c1
Raul Garcia <raul.gsouza@gmail.com>
2020-05-09 17:35:17 -0300

    Created Error class

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +20,7 @@ export default class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw Error('Email adress already used');
+      throw new AppError('Email adress already used', 400);
     }
 
     const hashedPassword = await hash(password, 8);
