e47428e9aed7e9245a90e0f230401639281a3829
Raul Garcia <raul.gsouza@gmail.com>
2020-05-15 19:38:38 -0300

    Created toast messages component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,13 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-
+    <ToastContainer />
     <AuthProvider>
       <SignIn />
     </AuthProvider>
