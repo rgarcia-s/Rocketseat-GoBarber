8f25fc9d5c888ea9a176a4825a672a32f76ad5ab
Raul Garcia <raul.gsouza@gmail.com>
2020-05-16 18:43:32 -0300

    Auto-removing toasts

--- frontend/src/components/ToastContainer/Toast/index.tsx
+++ frontend/src/components/ToastContainer/Toast/index.tsx
@@ -17,7 +17,7 @@ interface ToastProps {
 const icons = {
   info: <FiInfo size={24} />,
   error: <FiAlertCircle size={24} />,
-  success: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
 };
 
 const Toast: React.FC<ToastProps> = ({ message }) => {
