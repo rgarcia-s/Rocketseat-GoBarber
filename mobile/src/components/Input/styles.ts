a2bad88ee2e9cd45669d30d1ac065b26f3b86a68
Raul Garcia <raul.gsouza@gmail.com>
2020-05-25 12:15:57 -0300

    Using Yup to validate inputs

--- mobile/src/components/Input/styles.ts
+++ mobile/src/components/Input/styles.ts
@@ -3,6 +3,7 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 
 interface ContainerProps {
   isFocused: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.View<ContainerProps>`
@@ -13,6 +14,12 @@ export const Container = styled.View<ContainerProps>`
   border-radius: 10px;
   margin-bottom: 8px;
   border: 2px solid #232129;
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${(props) =>
     props.isFocused &&
