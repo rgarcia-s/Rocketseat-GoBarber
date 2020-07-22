a04ded44082fdebd2e0b584cf20262d19d8cd907
Raul Garcia <raul.gsouza@gmail.com>
2020-07-22 10:46:38 -0300

    Listing hours in day with appointment available

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderDayAvailability', () => {
   it('It should be able to list the day availability of a provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'provider',
-      date: new Date(2020, 6, 1, 8, 0, 0),
+      date: new Date(2020, 6, 1, 10, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'provider',
-      date: new Date(2020, 6, 1, 10, 0, 0),
+      date: new Date(2020, 6, 1, 11, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 6, 1, 9).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -33,8 +37,10 @@ describe('ListProviderDayAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
+        { hour: 11, available: false },
+        { hour: 12, available: true },
       ]),
     );
   });
