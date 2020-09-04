9f64357f73e4b16dc5a41189f8df7d1c2fd1f3e0
Raul Garcia <raul.gsouza@gmail.com>
2020-09-04 15:03:59 -0300

    Created pages and routes

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Profile from '../pages/Profile';
+
 import Dashboard from '../pages/Dashboard';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -13,7 +17,11 @@ const AppRoutes: React.FC = () => {
         cardStyle: { backgroundColor: '#312e38' },
       }}
     >
+      <App.Screen name="Profile" component={Profile} />
+
       <App.Screen name="Dashboard" component={Dashboard} />
+      <App.Screen name="CreateAppointment" component={CreateAppointment} />
+      <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
     </App.Navigator>
   );
 };
