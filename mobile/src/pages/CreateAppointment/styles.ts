5a9dd9fe33052a29841fd29b871c0f2e2687237c
Raul Garcia <raul.gsouza@gmail.com>
2020-09-05 14:59:41 -0300

    Added providers list in create appointment page

--- mobile/src/pages/CreateAppointment/styles.ts
+++ mobile/src/pages/CreateAppointment/styles.ts
@@ -1,4 +1,16 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { Provider } from './index';
+
+interface ProviderContainerProps {
+  selected: boolean;
+  last: boolean;
+}
+
+interface ProviderNameProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -27,4 +39,35 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
   margin-left: auto;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  background: ${(props) => (props.selected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: ${(props) => (props.last ? 48 : 16)}px;
+
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  height: 32px;
+  width: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${(props) => (props.selected ? '#232129' : '#f4ede8')};
 `;
